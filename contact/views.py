0a28d2b68a7d025be177d1584791b4e7cd2cd8fb
Bryan Guerra Cobos <bryan_9660@hotmail.com>
2025-07-28 17:18:54 +0200

    update: views.py import status

--- contact/views.py
+++ contact/views.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.contrib.auth.hashers import check_password
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import status, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .firebase import db
 import datetime
