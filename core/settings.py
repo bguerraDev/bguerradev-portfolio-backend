c045593d1515d31a6e49bade5d256b16d93f2e70
Bryan Guerra Cobos <bryan_9660@hotmail.com>
2025-07-28 22:13:14 +0200

    update: settings.py and req.txt add cors headers

--- core/settings.py
+++ core/settings.py
@@ -50,6 +50,7 @@ FIREBASE_CREDENTIAL = {
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,7 +68,11 @@ REST_FRAMEWORK = {
     )
 }
 
+CORS_ALLOW_ALL_ORIGINS = True  # Solo para desarrollo. En producción, usa CORS_ALLOWED_ORIGINS
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
