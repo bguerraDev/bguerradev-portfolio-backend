3c28c267345cf37e8d7ed8efbcfc94f27eaccf30
Bryan Guerra Cobos <bryan_9660@hotmail.com>
2025-07-29 18:07:32 +0200

    update add new migrations 0002 and update models.py

--- contact/models.py
+++ contact/models.py
@@ -1,22 +1,29 @@
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
 from .firebase import db
 from django.contrib.auth.hashers import make_password
 
-class CustomUserManager:
-    def create_user(self, username, password, **extra_fields):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
         if not username:
             raise ValueError("The Username field must be set")
-        user_data = {"username": username, "password": make_password(password), **extra_fields}
-        db.collection("users").document().set(user_data)
-        return {"username": username, "id": db.collection("users").document().id}  # Return a mock object or ID
+        user_data = {
+            "username": username,
+            "password": make_password(password),
+            **extra_fields
+        }
+        # Store user data in Firestore
+        user_ref = db.collection("users").add(user_data)
+        return self.model(username=username, id=user_ref.id, **extra_fields)
 
-    # Remove _create_django_user since we’re not using Django’s ORM
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-class CustomUser:
-    username = None  # Remove field since it’s stored in Firestore
-    is_active = None
-    is_staff = None
+        return self.create_user(username, password, **extra_fields)
 
+class CustomUser(AbstractUser):
+    # Inherit from AbstractUser to get default fields
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'username'
@@ -25,11 +32,11 @@ class CustomUser:
     def __str__(self):
         return self.username or "Unknown"
 
-class Message:
-    name = None
-    email = None
-    message = None
-    timestamp = None
+class Message(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} - {self.email}"
